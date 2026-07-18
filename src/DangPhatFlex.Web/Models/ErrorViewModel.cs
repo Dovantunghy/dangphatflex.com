fb2f4d20af6116bf3566eedc9a10a0a3bdd035bf
SMO Media <smomediahy@gmail.com>
2026-07-18 22:28:08 +0700

    chore: scaffold ASP.NET Core MVC solution with Areas routing

---
+++ src/DangPhatFlex.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DangPhatFlex.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
